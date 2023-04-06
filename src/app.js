bc90678635dc6e6da81e35313855223f461204eb
Guilherme Oliveira <121904094+GuiOliveiraRamos@users.noreply.github.com>
2023-04-06 13:09:16 -0300

    implementação get e post tweets

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ app.use(cors());
 app.use(express.json());
 
 const usuario = [];
+const tweets = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+];
 
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
@@ -19,16 +27,21 @@ app.post("/sign-up", (req, res) => {
   res.send("Cadastrado com sucesso");
 });
 
-const tweets = [
-  {
-    username: "bobesponja",
-    avatar:
-      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-    tweet: "Eu amo hambúrguer de siri!",
-  },
-];
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+  const verificarUsuario = usuario.find((u) => u.username === username);
+  if (!verificarUsuario) {
+    res.send("UNAUTHORIZED");
+  }
+
+  tweets.push({ username, tweet });
+  res.send("OK");
+});
 
 app.get("/tweets", (req, res) => {
+  if (tweets.length === 0) {
+    res.send([]);
+  }
   res.send(tweets);
 });
 
